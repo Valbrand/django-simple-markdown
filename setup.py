ddbaff9bf3a5192b652c5302bfe46273555d77f5
Daniel Oliveira <dvalbrand@gmail.com>
2013-10-16 20:12:25 -0300

    Figuring setup.py out

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 # ~ le encoding: utf-8 ~
 
-from disutils.core import setup
+from distutils.core import setup
 
 setup(
 	name = 'django_simple_markdown',
-	version = '1.0',
-	packages = ['djsmd'],
+	version = '1.0.1',
+	description = 'Django package for markdown editing within forms. Uses simple-markdown.js',
+	packages = ['djsmd', 'djsmd/static/djsmd/js', 'djsmd/templates', 'djsmd/templatetags'],
 	requires = ['Markdown'],
 	author = 'Daniel Oliveira',
 	author_email = 'daniel@dvalbrand.com',
