fad38f9559d4572450080802d089f9e71d3b8a88
Daniel Oliveira <dvalbrand@gmail.com>
2013-10-17 00:36:47 -0300

    First successful setup

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 # ~ le encoding: utf-8 ~
 
-from setuptools import find_packages, setup
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
 
 setup(
 	name = 'django_simple_markdown',
-	version = '1.0.1',
+	version = '1.0.5',
 	description = 'Django package for markdown editing within forms. Uses simple-markdown.js',
-	packages = find_packages(),
+	package_dir = {
+		'django_simple_markdown' : 'djsmd'
+	},
+	packages = ['django_simple_markdown', 'django_simple_markdown.templatetags'],
+	package_data = {
+		'django_simple_markdown' : ['templates/*', 'static/djsmd/js/*']
+	},
 	requires = ['Markdown'],
+	install_requires = ['Markdown'],
 	author = 'Daniel Oliveira',
 	author_email = 'daniel@dvalbrand.com',
 	url = 'https://github.com/Valbrand/django-simple-markdown',
